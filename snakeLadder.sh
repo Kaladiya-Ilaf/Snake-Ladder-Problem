e9f7a5a98f1a6ba14a31c0561a2077be3eb1bd86
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-08-03 14:04:24 +0530

    Refactored : Dice counter for both player added.

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -4,6 +4,10 @@
 START_POSITION=0
 WIN_POSITION=100
 
+#Initializing counters
+counter1=0
+counter2=0
+
 #Assigning players to their initial position
 player1=$START_POSITION
 player2=$START_POSITION
@@ -37,14 +41,15 @@ function climbLadder(){
 
 function playGame(){
    player=$1
-	#Randomly getting the value  of dice after rolling to play
-	diceValue=$(( RANDOM % 6 + 1 ))
-	echo >&2 "Result of dice rolled : " $diceValue
+   counter=$2
+   #Randomly getting the value  of dice after rolling to play
+   diceValue=$(( RANDOM % 6 + 1 ))
+   echo >&2 "Result of dice rolled : " $diceValue
+   ((counter++))
 
-	#Randomly generating  option to decide further action to play
-	playOption=$(( RANDOM % 3 ))
-
-	case $playOption in
+   #Randomly generating  option to decide further action to play
+   playOption=$(( RANDOM % 3 ))
+   case $playOption in
       0) echo >&2 "Option : No Play.";;
 
       1) echo >&2 "Option : Sanke Bite"
@@ -52,16 +57,28 @@ function playGame(){
 
       2) echo >&2 "Option : Climb Ladder"
          player=$(climbLadder $player $diceValue)
-         player=$(playGame $player);;
+         IFS="$(printf '\3')"
+         set -- $(playGame $player $counter)
+         player=$1
+         counter=$2;;
+
    esac
-	echo $player
+   printf '%d\3' "$player" "$counter"
 }
+
 
 while [[ $players1 -lt $WIN_POSITION ]] && [[ $player2 -lt $WIN_POSITION ]]
 do
-   player1=$(playGame $player1)
+   IFS="$(printf '\3')"
+   set -- $(playGame $player1 $counter1)
+   player1="$1"
+   counter1="$2"
    echo -e  "Position of player1 is $player1 \n"
-   player2=$(playGame $player2)
+
+   IFS="$(printf '\3')"
+   set -- $(playGame $player2 $counter2)
+   player2="$1"
+   counter2="$2"
    echo -e  "Position of player2 is $player2 \n"
 done
 
@@ -71,4 +88,7 @@ then
 else
    echo "Player2 is the winner!!"
 fi
+
+echo count1 : $counter1
+echo count2 : $counter2
 
